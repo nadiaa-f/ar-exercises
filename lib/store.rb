bd1dbd74f89c1276ca31c69c4fc7a8ffd983c192
Nadia Fatah <nadi.fatah@gmail.com>
2022-02-04 19:13:55 +0000

    Exercise 7

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,7 @@
 class Store < ActiveRecord::Base
-
+  has_many :employees
+  
+  validates :annual_revenue, presence: true, numericality: { greater_than_or_equal_to: 0, only_integer: true} 
+ 
+  validates :name, presence: true, length: { minimum: 3 }
 end
